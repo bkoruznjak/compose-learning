39c31b3afb8f115eb61b5679078b9e44648de072
bkoruznjak <borna.koruznjak@gmail.com>
2021-12-15 14:41:31 +0100

    Add a custom modifier to experiment with how custom composables are made

--- app/src/main/java/hr/flowable/composeplayground/compose/LayoutsInJetpackCompose.kt
+++ app/src/main/java/hr/flowable/composeplayground/compose/LayoutsInJetpackCompose.kt
@@ -17,8 +17,12 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.AlignmentLine
+import androidx.compose.ui.layout.FirstBaseline
+import androidx.compose.ui.layout.layout
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.ExperimentalUnitApi
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberImagePainter
@@ -196,8 +200,29 @@ fun LazyListThatScrolls() {
             }
         }
     }
-
 }
+
+/**
+ * Custom modifier that gets created only after constraints are known because composables can be
+ * measured only once due to performance
+ */
+
+fun Modifier.firstBaselineToTop(
+    firstBaselineToTop: Dp
+) = this.then(
+    layout { measurable, constraints ->
+        val placeable = measurable.measure(constraints)
+
+        check(placeable[FirstBaseline] != AlignmentLine.Unspecified)
+        val firstBaseline = placeable[FirstBaseline]
+        // Height of the composable with padding - first baseline
+        val placeableY = firstBaselineToTop.roundToPx() - firstBaseline
+        val height = placeable.height + placeableY
+        layout(placeable.width, height) {
+            placeable.placeRelative(0, placeableY)
+        }
+    }
+)
 
 @ExperimentalUnitApi
 @Preview(showBackground = false)
