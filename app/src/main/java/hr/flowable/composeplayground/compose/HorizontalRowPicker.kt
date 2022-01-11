cbb2fefd76a6ca552cd422a5c8910928ee8c8df9
bkoruznjak <borna.koruznjak@gmail.com>
2022-01-11 10:26:43 +0100

    More cleanup and refactoring

--- app/src/main/java/hr/flowable/composeplayground/compose/HorizontalRowPicker.kt
+++ app/src/main/java/hr/flowable/composeplayground/compose/HorizontalRowPicker.kt
@@ -5,13 +5,14 @@ import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -20,14 +21,13 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.boundsInWindow
 import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.layout.positionInWindow
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.dp
 
-val data = listOf("Pounds", "Kilograms", "Stones")
+val data = listOf("Pounds", "Kilograms", "Stones","Babu","gabu","sahiibiri","lolo","jahsdsda")
 
 private const val NO_INDEX = -1
 
@@ -39,7 +39,7 @@ data class MeasurementState(
 @Composable
 fun HorizontalRowPicker() {
   var state by remember {
-    mutableStateOf(MeasurementState(2))
+    mutableStateOf(MeasurementState(5))
   }
 
   CenteredSelectionMenu(state.selectedIndex, state.previousIndex) {
@@ -64,15 +64,11 @@ private fun CenteredSelectionMenu(
 
   val halfScreenWidth = screenWidthInInPx * 0.5f
 
-  val scrollState = rememberLazyListState()
+  val scrollState = rememberScrollState()
 
   val itemOffsetsFromParentCenterInPx by remember {
     mutableStateOf(
-      mutableMapOf<Int, Float?>(
-        0 to null,
-        1 to null,
-        2 to null
-      )
+      mutableMapOf<Int, Float?>()
     )
   }
 
@@ -86,32 +82,31 @@ private fun CenteredSelectionMenu(
     }
   )
 
-  LazyRow(
-    state = scrollState,
+  Row(
     modifier = Modifier
       .fillMaxWidth()
+      .horizontalScroll(scrollState,false) //enable scroll once you figure out how to update offsets on drag
       .offset(x = floatOffset.dp),
     horizontalArrangement = Arrangement.Center
   ) {
-
-    items(count = data.count()) { index ->
+    data.forEachIndexed { index, data ->
       Cell(
-        text = data[index],
+        text = data,
         isSelected = index == selectedIndex,
         onItemClick = { onItemClick(index) },
         modifier = Modifier.onGloballyPositioned {
           if (itemOffsetsFromParentCenterInPx[index] == null) {
             //Item center offsets from window center are calculated once and remembered
             val positionOnScreen = it.positionInWindow()
-            val screenHalf = halfScreenWidth
-            val halfItemWidth = it.boundsInWindow().width * 0.5f
-            val offsetInPixels = screenHalf - (positionOnScreen.x + halfItemWidth)
+            val halfItemWidth = it.size.width * 0.5f
+            val offsetInPixels = halfScreenWidth - (positionOnScreen.x + halfItemWidth)
             itemOffsetsFromParentCenterInPx[index] = with(density) {
               offsetInPixels.toDp().value
             }
           }
         })
     }
+
   }
 }
 
